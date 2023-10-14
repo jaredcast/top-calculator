556d55860f3deb816a6f1c766e03e253b6046d3e
jaredcast <jaredcast97@gmail.com>
2023-10-13 17:42:26 -0700

    Working text input from buttons

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log("Hello world!!!!!!");
+const buttons = document.querySelectorAll('button');
+const cur = document.querySelector('.cur')
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        //console.log(button.dataset.id);
+        appendNum(button.value);
+        // cur.textContent = button.value;
+    })}
+);
+
+let appendNum = (num) => {
+    if (cur.textContent === '0') {
+        cur.textContent = num;
+    }
+    else {
+        cur.textContent += num;
+    }
+}
